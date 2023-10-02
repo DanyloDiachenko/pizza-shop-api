31178c90775fcd9132eb42fedaadc478ae231065
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2023-10-02 21:16:59 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: "http://localhost:3000",
+    });
     app.setGlobalPrefix("api");
 
     const config = new DocumentBuilder()
