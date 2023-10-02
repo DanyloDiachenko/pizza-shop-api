7f6f15ed541dfeb89005e3d9a1ee084660980b88
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2023-10-02 20:55:45 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors({
-        origin: [
-            "http://localhost:3000",
-            // add prod frontend
-        ],
-        methods: ["GET", "POST"],
-    });
+    app.enableCors({ origin: true });
     app.setGlobalPrefix("api");
 
     const config = new DocumentBuilder()
