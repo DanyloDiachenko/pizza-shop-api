fff81f8044dd5277344f6f7385e58d95c3a369e2
Danylo Diachenko <danildiachenko23@gmail.com>
2023-11-28 22:29:51 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: true,
+        origin: ["http://localhost:3000", "https://pizza-shop-api-jade.vercel.app/"],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
     });
 
     app.setGlobalPrefix('api');
